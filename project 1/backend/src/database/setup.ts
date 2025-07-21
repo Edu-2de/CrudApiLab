8483389ad1b8f8ead76c67dc335e6f6cadff53c0
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-21 11:51:55 -0300

    Implement table existence check in checkIfTableExist function

--- project 1/backend/src/database/setup.ts
+++ project 1/backend/src/database/setup.ts
@@ -2,10 +2,16 @@ import pool from './connection';
 import fs from 'fs';
 import path from 'path';
 
-export const checkIfTableExist = async() =>{
-  try{
-    
-  }catch(error){
-
+export const checkIfTableExist = async () => {
+  try {
+    const result = await pool.query(`
+      SELECT EXISTS(
+        SELECT FROM CrudApiProject1_schema.tables
+        WHERE table_schema = 'public'
+        AND table_name = 'users');
+    `);
+    return result.rows[0].exists;
+  } catch (error) {
+    return false;
   }
-}
+};
