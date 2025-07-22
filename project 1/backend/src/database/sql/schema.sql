17ec0c221ff962e67b836a0f59b25128df023592
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-22 19:29:16 -0300

    Add initial user data insertion to users table in schema.sql

--- project 1/backend/src/database/sql/schema.sql
+++ project 1/backend/src/database/sql/schema.sql
@@ -5,6 +5,10 @@ CREATE TABLE IF NOT EXISTS users (
   email VARCHAR(100) UNIQUE NOT NULL,
   password_hash VARCHAR(255) NOT NULL,
   role VARCHAR(20) DEFAULT 'user' CHECK(role IN ('full_access', 'limit_access', 'user')),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
+
+INSERT INTO users (first_name, second_name, email, password_hash, role)VALUES
+('Alex', 'Mind', 'alexmind@gmail.com', '$2b$10$hxrKnQby/FyLhdEjrOpc5OqUg2t9hvQ66tAPTq1InkPtnOlb4Spn6', 'full_access')
+ON CONFLICT (email) DO NOTHING;
