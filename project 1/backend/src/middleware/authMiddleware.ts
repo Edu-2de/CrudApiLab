a774b9ca30183ec3a369855c0cc30b06fc2fa776
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-23 13:17:39 -0300

    feat: implement token authentication logic in AuthMiddleware

--- project 1/backend/src/middleware/authMiddleware.ts
+++ project 1/backend/src/middleware/authMiddleware.ts
@@ -10,3 +10,12 @@ interface AuthRequest extends Request{
 }
 
 const JWT_SECRET = process.env.JWT_SECRET || '';
+
+export class AuthMiddleware {
+  static authenticateToken = (req: AuthRequest, res: Response, next: NextFunction): void => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    
+  }
+}
