c6db18912d241ee0be1f66a58270b22744b966d0
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-23 13:26:35 -0300

    feat: integrate auth routes into the main application

--- project 1/backend/src/app.ts
+++ project 1/backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
 import { setupDB, testConnection } from './database/setup';
+import authRoutes from './routes/authRoutes';
 
 dotenv.config();
 const app = express();
@@ -12,9 +13,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-const startServer = async() =>{
-  try{
-    console.log('Starting server...')
+const startServer = async () => {
+  try {
+    console.log('Starting server...');
 
     await testConnection();
     console.log('Database connection successful');
@@ -22,15 +23,16 @@ const startServer = async() =>{
     await setupDB();
     console.log('Database setup completed');
 
-    app.listen(PORT, ()=>{
+    app.use('/user', authRoutes);
+
+    app.listen(PORT, () => {
       console.log('Server running on port: ' + PORT);
       console.log(`Test DB: http://localhost:${PORT}/test-db`);
-      
     });
-  }catch(error){
+  } catch (error) {
     console.error('Failed to start server:', error);
-    process.exit(1)
+    process.exit(1);
   }
 };
 
-startServer()
+startServer();
