b222c4c5365e51b41c9399a429923b119f6c8e0f
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-23 12:48:34 -0300

    feat: implement login method in AuthController with email and password validation

--- project 1/backend/src/controllers/authController.ts
+++ project 1/backend/src/controllers/authController.ts
@@ -5,5 +5,16 @@ import pool from '../database/connection';
 const JWT_SECRET = process.env.JWT_SECRET || '';
 
 export class AuthController {
-  
+  static login = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { email, password } = req.body;
+      if (!email || !password) {
+        res.status(400).json({ error: 'Email or password is missing!' });
+        return;
+      }
+      const result = await pool.query(
+        
+      )
+    } catch (error) {}
+  };
 }
