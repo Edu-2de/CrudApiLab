2834d02b768592d661b92cf7685e5c9496051c4f
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-24 12:43:18 -0300

    feat: add routes for user retrieval and deletion with admin middleware

--- project 1/backend/src/routes/authRoutes.ts
+++ project 1/backend/src/routes/authRoutes.ts
@@ -7,4 +7,11 @@ const router = Router();
 router.post('/login', AuthController.login);
 router.post('/register', AuthController.register);
 
+router.get('/:userId', AuthMiddleware.requireAdmin, AuthController.getUserById);
+router.get('/all', AuthMiddleware.requireAdmin, AuthController.getAllUsers);
+
+router.put('/:userId', AuthMiddleware.requireAdmin, AuthController.updateUserById);
+
+router.delete('/:userId', AuthMiddleware.requireAdmin, AuthController.deleteUserById);
+
 export default router;
