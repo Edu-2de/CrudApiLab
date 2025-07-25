f1b7df0ae5d43cca814c1fb097ada100f226e467
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-25 19:16:44 -0300

    Refactor code structure for improved readability and maintainability

--- project 1/frontend/src/app/components/carrousel.tsx
+++ project 1/frontend/src/app/components/carrousel.tsx
@@ -1,18 +1,21 @@
 'use client';
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 export default function Carrousel() {
   const carrouselItems = [
     {
-      title: 'Welcome to Project 1',
-      description: 'The best crudApiLab',
-      backgroundColor: 'bg-amber-100',
+      src: "/photo1.jpg",
+      alt: "Description 1"
     },
     {
-      title: 'This is the Local',
-      description: 'The best crudApiLab',
-      backgroundColor: 'bg-amber-200',
+      src: "/photo2.jpg",
+      alt: "Description 2"
     },
+    {
+      src: "/photo3.jpg",
+      alt: "Description 3"
+    }
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -51,19 +54,14 @@ export default function Carrousel() {
   }, [nextSlide, prevSlide]);
 
   return (
-    <div className="relative w-screen min-h-[60vh] md:min-h-[70vh] flex items-center justify-center bg-neutral-100 px-0 py-0 mt-24 md:mt-30">
-      <div className="relative w-full mx-auto rounded-2xl overflow-hidden shadow-2xl bg-white/90">
-        <div
-          className="flex flex-col items-center justify-center h-[40vh] md:h-[60vh] px-6 md:px-24 transition-all duration-700"
-          style={{ backgroundColor: currentItem.backgroundColor, minHeight: 320 }}
-        >
-          <h2 className="text-4xl md:text-5xl font-extrabold text-neutral-800 mb-6 text-center drop-shadow-sm tracking-tight">
-            {currentItem.title}
-          </h2>
-          <p className="mt-2 text-2xl md:text-4xl text-neutral-700 text-center max-w-2xl font-medium">
-            {currentItem.description}
-          </p>
-        </div>
+    <div className="relative w-screen min-h-[60vh] md:min-h-[70vh] flex items-center justify-center bg-neutral-100 px-0 py-0">
+      <div className="relative w-full mx-auto overflow-hidden flex justify-center">
+        <Image className = "rounded-2xl"
+          src={currentItem.src}
+          alt={currentItem.alt}
+          width={1800}
+          height={600}
+        />
       </div>
     </div>
   );
