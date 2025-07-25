5673b54c32d822a89b8d71b48a1499ae2878b270
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-25 19:28:03 -0300

    feat: update carousel component with improved image handling and navigation controls

--- project 1/frontend/src/app/components/carrousel.tsx
+++ project 1/frontend/src/app/components/carrousel.tsx
@@ -5,17 +5,17 @@ import Image from 'next/image';
 export default function Carrousel() {
   const carrouselItems = [
     {
-      src: "/photo1.jpg",
-      alt: "Description 1"
+      src: '/photo1.jpg',
+      alt: 'Description 1',
     },
     {
-      src: "/photo2.jpg",
-      alt: "Description 2"
+      src: '/photo2.jpg',
+      alt: 'Description 2',
     },
     {
-      src: "/photo3.jpg",
-      alt: "Description 3"
-    }
+      src: '/photo3.jpg',
+      alt: 'Description 3',
+    },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -56,12 +56,28 @@ export default function Carrousel() {
   return (
     <div className="relative w-screen min-h-[60vh] md:min-h-[70vh] flex items-center justify-center bg-neutral-100 px-0 py-0">
       <div className="relative w-full mx-auto overflow-hidden flex justify-center">
-        <Image className = "rounded-2xl"
-          src={currentItem.src}
-          alt={currentItem.alt}
-          width={1800}
-          height={600}
-        />
+        <Image className="rounded-2xl" src={currentItem.src} alt={currentItem.alt} width={1800} height={600} />
+      </div>
+      {/* Controls */}
+      <div className="absolute top-1/2 left-0 w-full flex justify-between items-center px-6 -translate-y-1/12 pointer-events-none">
+        <div className="pointer-events-auto">
+          <button
+            onClick={prevSlide}
+            className="bg-white/90 hover:bg-neutral-200 text-neutral-700 border border-neutral-300 rounded-full w-14 h-14 flex items-center justify-center shadow cursor-pointer "
+            aria-label="prev"
+          >
+            <span className="text-3xl font-bold mb-1">&lt;</span>
+          </button>
+        </div>
+        <div className="pointer-events-auto">
+          <button
+            onClick={nextSlide}
+            className="bg-white/90 hover:bg-neutral-200 text-neutral-700 border border-neutral-300 rounded-full w-14 h-14 flex items-center justify-center shadow cursor-pointer "
+            aria-label="next"
+          >
+            <span className="text-3xl font-bold mb-1">&gt;</span>
+          </button>
+        </div>
       </div>
     </div>
   );
