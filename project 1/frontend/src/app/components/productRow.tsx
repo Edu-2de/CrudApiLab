9ad106332e7a6b81bf7801b5ce7a845aea1937f4
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-27 21:29:41 -0300

    feat: update productRow component to include layout structure

--- project 1/frontend/src/app/components/productRow.tsx
+++ project 1/frontend/src/app/components/productRow.tsx
@@ -32,7 +32,7 @@ export default function productRow() {
 
   return(
     <div className="relative w-screen min-h-[60vh] md:min-h-[70vh] flex items-center justify-center px-0 py-0 mt-24 md:mt-5 bg-amber-950">
-      <div className=""></div>
+      <div className="relative h-[70vh] w-full max-w-7xl mx-auto overflow-hidden bg-amber-600"></div>
     </div>
   )
 }
