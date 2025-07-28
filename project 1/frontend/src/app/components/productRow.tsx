371abca058dc5e7807b2350e8456774574f3695d
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-27 21:26:13 -0300

    feat: integrate productRow component into Home page layout

--- project 1/frontend/src/app/components/productRow.tsx
+++ project 1/frontend/src/app/components/productRow.tsx
@@ -31,6 +31,8 @@ export default function productRow() {
   ]
 
   return(
-    <div className="relative w-screen min-h-[60vh] md:min-h-[70vh] flex items-center justify-center px-0 py-0 mt-24 md:mt-20 bg-amber-950"></div>
+    <div className="relative w-screen min-h-[60vh] md:min-h-[70vh] flex items-center justify-center px-0 py-0 mt-24 md:mt-5 bg-amber-950">
+      <div className=""></div>
+    </div>
   )
 }
--- project 1/frontend/src/app/page.tsx
+++ project 1/frontend/src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import Carrousel from "./components/carrousel"
 import Header from "./components/header"
+import ProductRow from "./components/productRow"
 
 export default function Home() {
   return (
     <>
       <Header/>
       <Carrousel/>
+      <ProductRow/>
     </>
   );
 }
