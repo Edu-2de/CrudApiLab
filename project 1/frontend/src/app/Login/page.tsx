aa71da51b93d4cf3dd69d8266c362d528a6431b9
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-28 19:05:55 -0300

    feat: update registration form to include separate first and second name fields

--- project 1/frontend/src/app/Login/page.tsx
+++ project 1/frontend/src/app/Login/page.tsx
@@ -145,18 +145,27 @@ export default function LoginRegisterPage() {
           autoComplete="off"
         >
           {mode === "register" && (
-            <div className="relative">
+            <><div className="relative">
               <FiUser className="absolute left-3 top-3 text-gray-400" />
               <input
                 type="text"
-                placeholder="Your name"
+                placeholder="Your first name"
                 required
                 value={first_name}
-                onChange={e => setName(e.target.value)}
+                onChange={e => setfirst_name(e.target.value)}
                 className="pl-10 pr-3 py-2 rounded-lg border border-gray-200 w-full bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-100 transition input-text-black"
-                style={{ fontSize: "1.08rem" }}
-              />
-            </div>
+                style={{ fontSize: "1.08rem" }} />
+            </div><div className="relative">
+                <FiUser className="absolute left-3 top-3 text-gray-400" />
+                <input
+                  type="text"
+                  placeholder="Your second name"
+                  required
+                  value={second_name}
+                  onChange={e => setsecond_name(e.target.value)}
+                  className="pl-10 pr-3 py-2 rounded-lg border border-gray-200 w-full bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-100 transition input-text-black"
+                  style={{ fontSize: "1.08rem" }} />
+              </div></>
           )}
           <div className="relative">
             <FiMail className="absolute left-3 top-3 text-gray-400" />
