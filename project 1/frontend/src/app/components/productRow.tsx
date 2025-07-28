2c047df485811a86b794846f06a363540d4ab9c7
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-27 21:39:28 -0300

    feat: refactor ProductRow component for improved styling and structure

--- project 1/frontend/src/app/components/productRow.tsx
+++ project 1/frontend/src/app/components/productRow.tsx
@@ -1,54 +1,54 @@
 "use client";
 
 const cardColors = [
-  "bg-neutral-100",
-  "bg-neutral-200",
-  "bg-neutral-300",
-  "bg-neutral-400"
+  "bg-[#8c9ca2]",
+  "bg-[#5c6150]",
+  "bg-[#8c8b91]",
+  "bg-[#8c8584]"
 ];
 
-export default function productRow() {
+export default function ProductRow() {
   const categories = [
     {
       title: 'Best Sellers',
       options: {
-        option1 : 'shop men',
-        option2 : 'shop women'
+        option1: 'shop men',
+        option2: 'shop women'
       }
     },
     {
       title: 'New Arrivals',
       options: {
-        option1 : 'shop men',
-        option2 : 'shop women'
+        option1: 'shop men',
+        option2: 'shop women'
       }
     },
     {
       title: 'Mens',
       options: {
-        option1 : 'shop men'
+        option1: 'shop men'
       }
     },
     {
       title: 'Womans',
       options: {
-        option1 : 'shop women'
+        option1: 'shop women'
       }
     },
-  ]
+  ];
 
-  return(
+  return (
     <div className="relative w-screen min-h-[60vh] md:min-h-[70vh] flex items-center justify-center px-0 py-0 mt-24 md:mt-5">
-      <div className="relative h-[70vh] w-full max-w-[95%] mx-auto overflow-hidden ">
+      <div className="relative h-[70vh] w-full max-w-[95%] mx-auto overflow-hidden flex gap-6  p-8">
         {categories.map((cat, idx) => (
           <div
             key={cat.title}
-            className={`rounded-lg shadow p-6 flex flex-col items-center min-w-[180px] ${cardColors[idx % cardColors.length]}`}
+            className={`rounded-lg shadow p-6 flex flex-col items-center min-w-[200px] justify-between ${cardColors[idx % cardColors.length]}`}
           >
-            <h3 className="font-bold text-lg mb-2">{cat.title}</h3>
-            <div className="flex flex-col gap-2">
+            <h3 className=" text-lg text-white mb-2">{cat.title}</h3>
+            <div className="flex flex-col gap-2 w-2xl">
               {Object.values(cat.options).map((opt, i) => (
-                <button key={i} className="bg-amber-500 text-white px-4 py-2 rounded hover:bg-amber-700 transition">
+                <button key={i} className=" text-gray-800 px-4 py-2 rounded transition">
                   {opt}
                 </button>
               ))}
@@ -57,5 +57,5 @@ export default function productRow() {
         ))}
       </div>
     </div>
-  )
+  );
 }
