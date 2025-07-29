e605736012d8afbd70752f6472d934c96b3bd680
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-29 12:32:07 -0300

    feat: implement InformationCards component and integrate it into the Home page

--- project 1/frontend/src/app/page.tsx
+++ project 1/frontend/src/app/page.tsx
@@ -3,12 +3,14 @@ import Carrousel from "./components/carrousel"
 import Header from "./components/header"
 import ProductRow from "./components/shoesRow"
 import HighlightsProductRow from "./components/highlightsProductRow"
+import InformationCards from "./components/informationCards"
 
 export default function Home() {
   return (
     <>
       <Header/>
       <Carrousel/>
+      <InformationCards/>
       <HighlightsProductRow/>
       <ProductRow/>
     </>
