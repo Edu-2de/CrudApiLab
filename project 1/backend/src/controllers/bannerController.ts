6094de78bbbd17ba56dd1497ccdab78a9755e86d
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-30 18:13:52 -0300

    fix: improve error handling and formatting in BannerController add method

--- project 1/backend/src/controllers/bannerController.ts
+++ project 1/backend/src/controllers/bannerController.ts
@@ -3,16 +3,22 @@ import pool from '../database/connection';
 
 const JWT_SECRET = process.env.JWT_SECRET || '';
 
-export class BannerController{
-  static add = async(req:Request, res:Response): Promise<void> => {
-    try{
-      const {title, image_url, link_url} = req.body
-      if(!title || !image_url || !link_url){
-        res.status(400).json({message: 'Any of arguments of the banner is missing'})
+export class BannerController {
+  static add = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { title, image_url, link_url } = req.body;
+      if (!title || !image_url || !link_url) {
+        res.status(400).json({ message: 'Any of arguments of the banner is missing' });
         return;
       }
-    }catch(error){
 
-    }
-  }
+      const bannerResult = await pool.query(`SELECT * FROM banners WHERE image_url  = $1`, [image_url]);
+      if(bannerResult.rows.length !== 0){
+        res.status(400).json({message: 'There is already an image with this url'});
+        return;
+      }
+
+      
+    } catch (error) {}
+  };
 }
