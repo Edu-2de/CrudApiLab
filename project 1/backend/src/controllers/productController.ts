1e1e06faf4cd3254a914eaf98421503e44a47149
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-31 12:53:46 -0300

    fix: correct unique constraint placement for name fields in categories and products tables

--- project 1/backend/src/controllers/productController.ts
+++ project 1/backend/src/controllers/productController.ts
@@ -3,6 +3,13 @@ import pool from '../database/connection';
 
 export class ProductController {
   static add = async (req: Request, res: Response): Promise<void> => {
-    
+    try {
+      const { name, description, price, stock, category, image_url } = req.body;
+      if (!name || !description || !price || !stock || !category || !image_url) {
+        res.status(400).json({ message: 'Any of arguments is missing' });
+        return;
+      }
+      
+    } catch (error) {}
   };
 }
