68b57da6749b23367791bb14f96f0852e3db8d3d
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-31 12:49:50 -0300

    feat: implement ProductController with add method skeleton

---
+++ project 1/backend/src/controllers/productController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express';
+import pool from '../database/connection';
+
+export class ProductController {
+  static add = async (req: Request, res: Response): Promise<void> => {
+    
+  };
+}
