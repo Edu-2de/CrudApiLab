ce350a767a35781282ba834187bb83d15f236ab0
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-31 11:56:10 -0300

    fix: correct HTTP method for deleting a banner and ensure proper route structure

--- project 1/backend/src/routes/bannerRoutes.ts
+++ project 1/backend/src/routes/bannerRoutes.ts
@@ -5,6 +5,13 @@ import { AuthMiddleware } from '../middleware/authMiddleware';
 const router = Router();
 
 router.post('/', AuthMiddleware.requireAdmin, BannerController.add);
-router.post('/:bannerId', AuthMiddleware.requireAdmin, BannerController.deleteBannerById);
+router.delete('/:bannerId', AuthMiddleware.requireAdmin, BannerController.deleteBannerById);
 
 router.get('/:bannerId', AuthMiddleware.requireAdmin, BannerController.getBannerById);
+router.get('/active', AuthMiddleware.requireAdmin, BannerController.getActiveBanners);
+router.get('/all', AuthMiddleware.requireAdmin, BannerController.getAllBanners);
+
+router.patch('/active/:bannerId', AuthMiddleware.requireAdmin, BannerController.activeBannerById);
+router.patch('/disable/:bannerId', AuthMiddleware.requireAdmin, BannerController.disableBannerById);
+
+export default router;
