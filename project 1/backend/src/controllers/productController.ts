7cbc8a2bf2b0c0b095abc7aee49097d020239581
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-07-31 13:05:46 -0300

    fix: improve stock validation and ensure proper category handling in product addition

--- project 1/backend/src/controllers/productController.ts
+++ project 1/backend/src/controllers/productController.ts
@@ -21,12 +21,34 @@ export class ProductController {
         return;
       }
 
-      if (stock <= 0 || stock > 9999){
-        res.status(400).json({message: 'Invalid stock'});
+      if (stock <= 0 || stock > 9999) {
+        res.status(400).json({ message: 'Invalid stock' });
         return;
       }
 
-      
-    } catch (error) {}
+      const categoryCheckResult = await pool.query(`SELECT * FROM categories WHERE name = $1`, [category]);
+      if (categoryCheckResult.rows.length === 0) {
+        res.status(400).json({ message: 'This category not exist' });
+        return;
+      }
+
+      const categoryResult = categoryCheckResult.rows[0];
+      const categoryId = categoryResult.id;
+
+      const productAddResult = await pool.query(
+        `INSERT INTO products(name, description, price, stock, category_id, image_url, created_at) VALUES($1, $2, $3, $4, $5, $6, CURRENT_TIMESTAMP)`,
+        [name, description, price, stock, categoryId, image_url]
+      );
+
+      res.status(201).json({
+        message: 'Product added successfully',
+        product: productAddResult.rows[0],
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Error during banner adding',
+        error: error instanceof Error ? error.message : String(error),
+      });
+    }
   };
 }
