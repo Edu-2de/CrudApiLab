75bd4813c41a2ca974244f0630b9786c80a4708a
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-01 12:44:48 -0300

    feat: implement addProductImageById method with basic validation for product ID

--- project 1/backend/src/controllers/productImagesController.ts
+++ project 1/backend/src/controllers/productImagesController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
 import pool from '../database/connection';
 
-export class ProductImagesController{
-  static addProductImage = async(req: Request, res: Response): Promise<void> => {
-    try{
-
-    }catch(error){
+export class ProductImagesController {
+  static addProductImageById = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const productId = Number(req.params.productId);
+      if (!productId) {
+        res.status(400).json({ message: 'Product id is missing' });
+        return;
+      }
       
-    }
-  }
+    } catch (error) {}
+  };
 }
