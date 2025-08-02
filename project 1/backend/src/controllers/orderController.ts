4b5dc09ded43948f7029d5b1d0531741defc210f
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-02 19:46:12 -0300

    fix: enhance addOrder method to include user authentication and total validation

--- project 1/backend/src/controllers/orderController.ts
+++ project 1/backend/src/controllers/orderController.ts
@@ -2,11 +2,19 @@ import { Request, Response } from 'express';
 import pool from '../database/connection';
 
 export class OrderController {
-  static addOrder = async (req: Request, res: Response): Promise<void> => {
+  static addOrder = async (req: any, res: Response): Promise<void> => {
     try {
-      
-    } catch (error) {
+      const userId = req.user.id;
+      if (!userId) {
+        res.status(400).json({ message: 'you need to be logged in for this action' });
+        return;
+      }
 
-    }
+      const { total } = req.body;
+      if (!total) {
+        res.status(400).json({ message: 'Total is missing' });
+        return;
+      }
+    } catch (error) {}
   };
 }
