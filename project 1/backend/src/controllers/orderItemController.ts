af588f9b07b96389c39c5ad131dcb49a7875f342
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-02 20:22:47 -0300

    feat: implement addOrderItem method in OrderItemController

---
+++ project 1/backend/src/controllers/orderItemController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import pool from '../database/connection';
+
+export class OrderItemController {
+  static addOrderItem = async (req: Request, res: Response): Promise<void> => {
+    try {
+
+    } catch (error) {
+      
+    }
+  };
+}
