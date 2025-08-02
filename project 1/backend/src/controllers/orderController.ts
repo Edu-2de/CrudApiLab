d521b3c200eccd480a98f05d7d1068e239d28aa1
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-02 19:42:17 -0300

    feat: implement addOrder method in OrderController for order processing

---
+++ project 1/backend/src/controllers/orderController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import pool from '../database/connection';
+
+export class OrderController {
+  static addOrder = async (req: Request, res: Response): Promise<void> => {
+    try {
+      
+    } catch (error) {
+
+    }
+  };
+}
