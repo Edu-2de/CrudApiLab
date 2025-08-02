8d3e061f1c08e26937374bcad0e6fc3030df917e
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-02 20:15:13 -0300

    fix: add order ID validation in deleteOrderById method

--- project 1/backend/src/controllers/orderController.ts
+++ project 1/backend/src/controllers/orderController.ts
@@ -145,10 +145,14 @@ export class OrderController {
     }
   };
   static deleteOrderById = async (req: Request, res: Response): Promise<void> => {
-    try{
-      
-    }catch(error){
+    try {
+      const orderId = Number(req.params.orderId);
+      if (!orderId) {
+        res.status(400).json({ message: 'Order id is missing' });
+        return;
+      }
 
-    }
-  }
+      
+    } catch (error) {}
+  };
 }
