d24b36b234e0b789fddaa94f35e087d38749eb53
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-02 20:25:09 -0300

    fix: add order ID validation in addOrderItem method

--- project 1/backend/src/controllers/orderItemController.ts
+++ project 1/backend/src/controllers/orderItemController.ts
@@ -4,9 +4,13 @@ import pool from '../database/connection';
 export class OrderItemController {
   static addOrderItem = async (req: Request, res: Response): Promise<void> => {
     try {
+      const orderId = Number(req.params.orderId);
+      if (orderId) {
+        res.status(400).json({ message: 'Order id is missing' });
+        return;
+      }
 
-    } catch (error) {
       
-    }
+    } catch (error) {}
   };
 }
