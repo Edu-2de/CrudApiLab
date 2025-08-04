f369de62c61daf9769cb1c7938b65751c6c7182c
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-04 19:10:17 -0300

    feat: implement Login layout component and update Home page imports

--- project 1/frontend/src/app/page.tsx
+++ project 1/frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 import Carrousel from "./components/carrousel"
-import Header from "./components/header"
 import HighlightsProductRow from "./components/highlightsProductRow"
 import InformationCards from "./components/informationCards"
 import TopPartsRow from "./components/topPartsRow"
 import ShoesRow from "./components/shoesRow"
 import BottomParts from "./components/bottomParts"
 import Footer from "./components/footer"
+import Header from './components/header';
 
 export default function Home() {
   return (
