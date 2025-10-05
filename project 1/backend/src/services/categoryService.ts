4fd590b1b6070106dec18d047df028e3f8c2b7b5
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-10-05 14:11:48 -0300

    style: format code for consistency in category service

--- project 1/backend/src/services/categoryService.ts
+++ project 1/backend/src/services/categoryService.ts
@@ -1,4 +1,20 @@
-import {CreateCategoryDto, UpdateCategoryDto} from '../dtos/category.dto';
+import { CreateCategoryDto, UpdateCategoryDto } from '../dtos/category.dto';
 import { CategoryRepository } from '../repositories/categoryRepository';
 import { Category } from '../entities/Category';
 import { AppError } from '../utils/appError';
+
+export class CategoryService {
+  static async createCategory(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const { title, description } = createCategoryDto;
+
+    const categoryData = {
+      name: title,
+      description,
+    };
+
+    const category = await CategoryRepository.create(categoryData);
+    return category;
+  }
+
+  
+}
