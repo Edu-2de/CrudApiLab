84217ccddba7b5268ce16e3fe5bcfffc2a76f605
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-10-05 14:09:09 -0300

    feat: add initial imports for category service functionality

---
+++ project 1/backend/src/services/categoryService.ts
@@ -0,0 +1,4 @@
+import {CreateCategoryDto, UpdateCategoryDto} from '../dtos/category.dto';
+import { CategoryRepository } from '../repositories/categoryRepository';
+import { Category } from '../entities/Category';
+import { AppError } from '../utils/appError';
