d87b8787a3de90ca4eda0b830167ebdf76333aff
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-10-06 14:25:31 -0300

    refactor: add class structure for CategoryController

--- project 1/backend/src/controllers/categoryController.ts
+++ project 1/backend/src/controllers/categoryController.ts
@@ -2,3 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { CategoryRepository } from "../repositories/categoryRepository";
 import { sendSuccess } from "../utils/response";
 import { CategoryParamsDto, CreateCategoryDto, UpdateCategoryDto } from "../dtos/category.dto";
+
+export class CategoryController{
+  
+}
