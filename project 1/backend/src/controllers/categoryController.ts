37fd5c836c6e6c5834e5fc028ab2cdac77c61155
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-10-06 14:29:39 -0300

    feat: add return type to createCategory method and implement getAllCategories method in CategoryController

--- project 1/backend/src/controllers/categoryController.ts
+++ project 1/backend/src/controllers/categoryController.ts
@@ -4,10 +4,19 @@ import { sendSuccess } from "../utils/response";
 import { CategoryParamsDto, CreateCategoryDto, UpdateCategoryDto } from "../dtos/category.dto";
 
 export class CategoryController{
-  static async createCategory(req: Request, res: Response, next: NextFunction){
+  static async createCategory(req: Request, res: Response, next: NextFunction): Promise<void>{
     try{
       const result = await CategoryService.createCategory(req.validatedData as CreateCategoryDto);
       sendSuccess(res, result, 'Category added successful');
+    }catch(err){
+      next(err);
+    }
+  }
+
+  static async getAllCategories(req: Request, res: Response, next: NextFunction): Promise<void>{
+    try{
+      const categories = await CategoryService.getAllCategories();
+      sendSuccess(res, {categories}, 'Categories retrieved successfully')
     }catch(err){
       next(err);
     }
