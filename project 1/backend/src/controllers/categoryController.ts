11f667a8e6ab8946d3801536dac7d3c1f0cba19f
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-10-06 14:27:44 -0300

    feat: update createCategory method to use CategoryService for category creation

--- project 1/backend/src/controllers/categoryController.ts
+++ project 1/backend/src/controllers/categoryController.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from "express";
-import { CategoryRepository } from "../repositories/categoryRepository";
+import { CategoryService } from "../services/categoryService";
 import { sendSuccess } from "../utils/response";
 import { CategoryParamsDto, CreateCategoryDto, UpdateCategoryDto } from "../dtos/category.dto";
 
 export class CategoryController{
   static async createCategory(req: Request, res: Response, next: NextFunction){
     try{
-
+      const result = await CategoryService.createCategory(req.validatedData as CreateCategoryDto);
+      sendSuccess(res, result, 'Category added successful');
     }catch(err){
       next(err);
     }
