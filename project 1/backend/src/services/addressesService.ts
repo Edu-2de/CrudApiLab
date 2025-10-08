dd52cfc2783726aeb8e08ab0213daac6b5619c72
Eduardo Da Silva Paim <edupaim1712@gmail.com>
2025-10-08 19:21:29 -0300

    Add addressesService.ts with necessary imports

---
+++ project 1/backend/src/services/addressesService.ts
@@ -0,0 +1,4 @@
+import {CreateAddressDto, UpdateAddressDto} from '../dtos/addresses.dto';
+import {AddressesRepository} from '../repositories/addressesRepository';
+import { Address } from '../entities/Address';
+import { AppError } from '../utils/appError';
