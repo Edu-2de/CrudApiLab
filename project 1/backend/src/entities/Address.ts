b44a65c4950a3d66483491d53a742d2dbc6b937d
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-10-14 16:04:53 -0300

    fix: change address_line2 from optional to required in Address interface

--- project 1/backend/src/entities/Address.ts
+++ project 1/backend/src/entities/Address.ts
@@ -2,10 +2,22 @@ export interface Address{
   id: number,
   user_id: number, 
   address_line1: string,
-  address_line2?: string,
+  address_line2: string,
   city: string,
   state: string,
   postal_code: number,
   country: string,
   created_at: Date,
+}
+
+
+export interface CreateAddress {
+  id: number,
+  user_id: number, 
+  address_line1: string,
+  address_line2?: string,
+  city: string,
+  state: string,
+  postal_code: number,
+  country: string,
 }
