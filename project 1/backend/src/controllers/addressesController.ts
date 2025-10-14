b31f5bee9b9e36e3f3aff38011c99a9309b3a74d
EDUARDO DA SILVA PAIM <85540781015@senacrs.edu.br>
2025-10-14 19:36:31 -0300

    refactor: remove unused AddressRepository and addressesService files

--- project 1/backend/src/controllers/addressesController.ts
+++ project 1/backend/src/controllers/addressesController.ts
@@ -1,6 +1,8 @@
 import {Request, Response} from 'express';
 import pool from '../database/connection';
-
+import { AddressService } from '../services/addressService';
+import { sendSuccess } from '../utils/response';
+import { CreateAddressDto, UpdateAddressDto } from '../dtos/addresses.dto';
 
 export class AddressesControler{
   static addAddress = async(req: Request, res: Response): Promise<void> => {
--- project 1/backend/src/services/addressesService.ts
+++
@@ -1,4 +0,0 @@
-import {CreateAddressDto, UpdateAddressDto} from '../dtos/addresses.dto';
-import {AddressesRepository} from '../repositories/addressesRepository';
-import { Address } from '../entities/Address';
-import { AppError } from '../utils/appError';
