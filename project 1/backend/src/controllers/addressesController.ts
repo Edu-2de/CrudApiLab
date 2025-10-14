203ad738dc47e4d0e6b0ccda8f13b120fbad8c7c
EDUARDO DA SILVA PAIM <85540781015@senacrs.edu.br>
2025-10-14 19:39:44 -0300

    fix: ensure user ID validation throws an error for invalid input

--- project 1/backend/src/controllers/addressesController.ts
+++ project 1/backend/src/controllers/addressesController.ts
@@ -8,7 +8,22 @@ export class AddressesControler{
   static addAddress = async(req: Request, res: Response): Promise<void> => {
     const userId = Number(req.params.userId);
     if(!userId){
-      
+      throw new Error('Invalid user ID');
     }
+
+    const {address_line1, address_line2, city, state, postal_code, country} = req.body;
+
+    const newAddress: CreateAddressDto = {
+      user_id: userId,
+      address_line1,
+      address_line2,
+      city,
+      state,
+      postal_code,
+      country,
+    };
+
+    const createdAddress = await AddressService.createAddress(newAddress);
+    sendSuccess(res, createdAddress, 'Address added successfully');
   }
 }
